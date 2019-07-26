87b5cacf7ebe7780e495de7d8984ffe76f26e96e
SomyaGupta88 <guptasaumya668@gmail.com>
2019-07-26 21:44:35 +0530

    search_element_rotated_array_single_binary_search

--- rotated_single_binary_search.c
+++ rotated_single_binary_search.c
@@ -9,7 +9,10 @@ int main(int argc,char *argv[]) {
 	}
 	int ele=atoi(argv[argc-1]);
 	int m= binary_search(arr,0,k,ele);
-	printf("element found at index %d",m);
+	if(m==-1)
+		printf("ele not found");
+	else
+		printf("element found at index %d",m);
 	return 0;
 }
 
@@ -18,15 +21,15 @@ int binary_search(int arr[],int l,int h,int ele)
 	if(l>h)
 		return -1;
 	int mid=l+((h-l)/2);
-	if(arr[mid]>arr[l] && ele<arr[mid] && ele>arr[l])
-		return binary_search(arr,l,mid-1,ele);
-	else if(arr[mid]>arr[l] && ele>arr[mid] || ele<arr[l])
-		return binary_search(arr,mid+1,h,ele);
-	else if(arr[mid]<arr[l])
-		return binary_search(arr,l,mid-1,ele);
-	else if(arr[mid]==ele)
+	if(arr[mid]==ele)
 		return mid;
-	else if(l==h && arr[l]!=ele)
-		return -1;
+	if(arr[mid]>=arr[l]) {
+		       if(ele<=arr[mid] && ele>=arr[l])
+		return binary_search(arr,l,mid-1,ele);
+         return binary_search(arr,mid+1,h,ele);
+	}
+       if(ele>=arr[mid] && ele<=arr[h])
+		return binary_search(arr,mid+1,h,ele);
+	return binary_search(arr,l,mid-1,ele);
 }
 
