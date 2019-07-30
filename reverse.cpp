88f9e4132f5e3c8248cd74839e0d84c6ce90e9e7
SomyaGupta88 <guptasaumya668@gmail.com>
2019-07-31 00:41:41 +0530

    reverse of a string

--- reverse.cpp
+++ reverse.cpp
@@ -2,18 +2,25 @@
 #include <cstring>
 using namespace std;
 
+void reverse_str(char *str,int len);
+
 int main (int argc,char *argv[])
 {
 	char *str;
 	str=argv[1];
 	int len=strlen(str);
+	reverse_str(str,len);
+	cout<<str;
+	return 0;
+}
+
+void reverse_str(char *str,int len)
+{
 	for(int i=0;i<len/2;i++)
 	{
 		char temp=str[i];
 		str[i]=str[len-1-i];
 		str[len-1-i]=temp;
 	}
-	cout<<str;
-	return 0;
 }
 
