84e0e9e9bdb13e81137c050a05076dff7b1cf7ec
SomyaGupta88 <guptasaumya668@gmail.com>
2019-07-31 22:54:45 +0530

    10 standard functions implemented by self

--- standard_func.cpp
+++ standard_func.cpp
@@ -12,66 +12,63 @@ int str_len(char *str) {
 	return i;
 }
 
-/*void str_cat(char *str,char *str1,int len,int len1) {
-	char *str2;
+char *str_cat(char *str,char *str1) {
+	char *str2=new char[50];
 	int i=0,j=0;
-	for(i=0;i<len;i++) {
+	for(i=0;i<strlen(str);i++) {
 		str2[j]=str[i];
 		j++;
 	}
-	for(i=0;i<len1;i++) {
+	for(i=0;i<strlen(str1);i++) {
 		str2[j]=str1[i];
 		j++;
 	}
-	cout<<str2;
-}*/
+	return str2;
+}
 
-void str_uppr(char *str,int len)
+char *str_uppr(char *str)
 {
-	for(int i=0;i<len;i++) {
+	for(int i=0;i<strlen(str);i++) {
 		if(str[i]>=97 && str[i]<=122) {
 			str[i]-=32;
 		}
 	}
-	cout<<str<<"\n";
+	return str;
 }
 
-void str_lwr(char *str,int len)
+char *str_lwr(char *str)
 {
-	for(int i=0;i<len;i++) {
+	for(int i=0;i<strlen(str);i++) {
 		if(str[i]>=65 && str[i]<=91) {
 			str[i]+=32;
 		}
 	}
-	cout<<str<<"\n";
+	return str;
 }
 
-void str_cpy(char *str,char *str1) {
-	str1=str;
-	cout<<str1<<"\n";
+char *str_cpy(char *str,char *str1) {
+	str=str1;
+	return str;
 }
 
-void str_cmp(char *str,char *str1,int len,int len1) {
-	int flag=0;
-	if(len!=len1)
-		cout<<"strings are not equal \n";
+int str_cmp(char *str,char *str1) {
+	int flag=1;
+	if(strlen(str)!=strlen(str1))
+		flag=-1;
 	else {
-		for(int i=0;i<len;i++) {
+		for(int i=0;i<strlen(str);i++) {
 			if(str[i]!=str1[i]) {
-				flag=1;
+				flag=-1;
 				break;
 			}
 		}
-	if(flag)
-		cout<<"strings are not equal..."<<"\n";
-	else
-		cout<<"strings are equal \n";
 	}
+	return flag;
 }
 
-int str_chr(char *str,char *chr,int len) {
+int str_chr(char *str,char *chr) {
 	int ind=-1;
-	for(int i=0;i<len;i++) {
+	for(int i=0;i<strlen(str);i++) {
 		if(str[i]==chr[0]) {
 			ind=i;
 			break;
@@ -80,9 +77,9 @@ int str_chr(char *str,char *chr,int len) {
 	return ind;
 }
 
-int str_rchr(char *str,char *chr,int len) {
+int str_rchr(char *str,char *chr) {
 	int ind=-1;
-	for(int i=len-1;i>=0;i--) {
+	for(int i=strlen(str)-1;i>=0;i--) {
 		if(str[i]==chr[0]) {
 			ind=i;
 			break;
@@ -91,16 +88,16 @@ int str_rchr(char *str,char *chr,int len) {
 	return ind;
 }
 
-char *str_set(char *str,char *chr,int len) {
+char *str_set(char *str,char *chr) {
 	char *s_str;
 	s_str=str;
-	for(int i=0;i<len;i++) {
+	for(int i=0;i<strlen(str);i++) {
 		s_str[i]=chr[0];
 	}
 	return s_str;
 }
 
-char *str_dup(char *str,int len) {
+char *str_dup(char *str) {
 	char *d_str;
 	d_str=str;
 	return d_str;
@@ -115,19 +112,27 @@ int main(int argc,char *argv[]) {
 	cout<<len<<"\n";
 	int len1=str_len(str1);
 	cout<<len1<<"\n";
+	int o=str_cmp(str,str1);
+	if(o==1)
+		cout<<"strings are equal"<<endl;
+	else if(o==-1)
+		cout<<"strings are not equal"<<endl;
 
-//	str_cat(str,str1,len,len1);
-	str_cpy(str,str1);
-	str_uppr(str,len);
-	str_lwr(str,len);
-	str_cmp(str,str1,len,len1);
-	int ind=str_chr(str,chr,len);
-	cout<<ind<<"\n";
-	int rind=str_rchr(str,chr,len);
-	cout<<rind<<"\n";
-	char *s_str=str_set(str,chr,len);
+	char *c_str=str_cat(str,str1);
+	cout<<"concat str1 & str2- "<<c_str<<endl;
+	char *cp=str_cpy(str,str1);
+	cout<<"copy str1 in str- "<<cp<<endl;
+	char *u_str=str_uppr(str);
+	cout<<"uppercase- "<<u_str<<endl;
+	char *l_str=str_lwr(str);
+	cout<<"lowercase- "<<l_str<<endl;
+	int ind=str_chr(str,chr);
+	cout<<"char fund at index- "<<ind<<"\n";
+	int rind=str_rchr(str,chr);
+	cout<<"char found at index- "<<rind<<"\n";
+	char *s_str=str_set(str,chr);
 	cout<<s_str<<"\n";
-	char *d_str=str_dup(str,len);
+	char *d_str=str_dup(str);
 	cout<<d_str<<"\n";
 	return 0;
 }
